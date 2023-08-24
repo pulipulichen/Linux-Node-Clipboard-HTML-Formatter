39d0fcfa1c302cf24d6c5e3ff7560d8cf9cee3c6
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-08-24 11:30:31 +0800

    西元2023年08月24日 (週四) 11時30分31秒 CST

--- remove-html.js
+++ remove-html.js
@@ -4,6 +4,7 @@
 // console.log(text)
 
 let filePath = process.argv[2]
+let outputFilePath = process.argv[3]
 //console.log(filePath)
 
 import fs from 'fs'
@@ -15,9 +16,32 @@ if (!filePath || !fs.existsSync(filePath)) {
 
 let fileContent = fs.readFileSync(filePath, 'utf8')
 //console.log(fileContent)
+// fileContent = fileContent.replace(/\b(?!>)\b\n\b(?!<)/g, ' ')
+let lines = fileContent.split('\n')
+let output = ''
+for (let i = 0; i < lines.length; i++) {
+  let line = lines[i]
+  if (i === 0) {
+    output = lines[i]
+  }
+
+  if (output.endsWith('>') && line.startsWith('<') ) {
+    output = output + '\n' + line
+  }
+  else {
+    output = output + ' ' + line
+  }
+}
+fileContent = output
+
+let newLineList = ['><div', '><br', '><p', '><h', '><ul', '><li', '><ol']
+newLineList.forEach(search => {
+  fileContent = fileContent.split(search).join(search[0] + '\n' + search.slice(1))
+})
+
 let $ = cheerio.load(fileContent)
 
 fileContent = $('body').text().trim()
 
 
-fs.writeFileSync(filePath, fileContent , 'utf8')
+fs.writeFileSync(outputFilePath, fileContent , 'utf8')
